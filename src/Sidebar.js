5201d90e2a0c75c863f86775dc93be68c5d52666
Nitya Bharti <nityabharti123@gmail.com>
2023-04-20 16:48:35 +0530

    whatsapp-mern

---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Sidebar.css";
+
+function sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar_header">
+        <div className="sidebar_headerRight">{/* <DonuLargeIcon /> */}</div>
+      </div>
+    </div>
+  );
+}
+
+export default sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCUORWJ_TkOk0ywEqjmbp3B9FWTwSN3YBQ",
+  authDomain: "whatsapp-mern-1ef9e.firebaseapp.com",
+  projectId: "whatsapp-mern-1ef9e",
+  storageBucket: "whatsapp-mern-1ef9e.appspot.com",
+  messagingSenderId: "75971070467",
+  appId: "1:75971070467:web:dae78b8acc70b3a1ebdc36",
+};
