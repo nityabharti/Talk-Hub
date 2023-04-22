e165bcd1196fc7c6a1dc7e0dbba1bdccb8c14d9d
Nitya Bharti <nityabharti123@gmail.com>
2023-04-22 23:48:10 +0530

    chat_section

--- src/Chat.js
+++ src/Chat.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Chat.css";
 import { Avatar } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import IconButton from "@mui/material/IconButton";
+import { AttachFile, SearchOutlined } from "@mui/icons-material";
 
 function Chat() {
   return (
@@ -11,7 +14,26 @@ function Chat() {
           <h3>Room name</h3>
           <p>Last seen at .....</p>
         </div>
-        <div className="chat_headerRight"></div>
+        <div className="chat_headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat_body">
+        <p className="chat_message">
+          <span className="chat_name">Ambuj</span>
+          This is a message
+          <span className="chat_timestamp">
+            {new Date().toLocaleTimeString()}
+          </span>
+        </p>
       </div>
     </div>
   );
