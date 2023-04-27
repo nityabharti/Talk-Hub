b2d74127b897631a29fa28f523623227e21e7b15
Nitya Bharti <nityabharti123@gmail.com>
2023-04-27 18:13:35 +0530

    modify chat section

--- src/Chat.js
+++ src/Chat.js
@@ -3,7 +3,12 @@ import "./Chat.css";
 import { Avatar } from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import IconButton from "@mui/material/IconButton";
-import { AttachFile, SearchOutlined } from "@mui/icons-material";
+import {
+  AttachFile,
+  InsertEmoticon,
+  SearchOutlined,
+} from "@mui/icons-material";
+import MicIcon from "@mui/icons-material/Mic";
 
 function Chat() {
   return (
@@ -11,7 +16,7 @@ function Chat() {
       <div className="chat_header">
         <Avatar />
         <div className="chat_headerInfo">
-          <h3>Room name</h3>
+          <h3>Ambuj</h3>
           <p>Last seen at .....</p>
         </div>
         <div className="chat_headerRight">
@@ -34,6 +39,77 @@ function Chat() {
             {new Date().toLocaleTimeString()}
           </span>
         </p>
+        <p className="chat_message chat_reciever">
+          <span className="chat_name">You</span>
+          This is a message
+          <span className="chat_timestamp">
+            {new Date().toLocaleTimeString()}
+          </span>
+        </p>
+        <p className="chat_message">
+          <span className="chat_name">Ambuj</span>
+          This is a message
+          <span className="chat_timestamp">
+            {new Date().toLocaleTimeString()}
+          </span>
+        </p>
+        <p className="chat_message chat_reciever">
+          <span className="chat_name">You</span>
+          This is a message
+          <span className="chat_timestamp">
+            {new Date().toLocaleTimeString()}
+          </span>
+        </p>
+        <p className="chat_message chat_reciever">
+          <span className="chat_name">You</span>
+          This is a message
+          <span className="chat_timestamp">
+            {new Date().toLocaleTimeString()}
+          </span>
+        </p>
+        <p className="chat_message">
+          <span className="chat_name">Ambuj</span>
+          This is a message
+          <span className="chat_timestamp">
+            {new Date().toLocaleTimeString()}
+          </span>
+        </p>
+        <p className="chat_message">
+          <span className="chat_name">Ambuj</span>
+          This is a message
+          <span className="chat_timestamp">
+            {new Date().toLocaleTimeString()}
+          </span>
+        </p>
+        <p className="chat_message">
+          <span className="chat_name">Ambuj</span>
+          This is a message
+          <span className="chat_timestamp">
+            {new Date().toLocaleTimeString()}
+          </span>
+        </p>
+        <p className="chat_message chat_reciever">
+          <span className="chat_name">You</span>
+          This is a message
+          <span className="chat_timestamp">
+            {new Date().toLocaleTimeString()}
+          </span>
+        </p>
+        <p className="chat_message chat_reciever">
+          <span className="chat_name">You</span>
+          This is a message
+          <span className="chat_timestamp">
+            {new Date().toLocaleTimeString()}
+          </span>
+        </p>
+      </div>
+      <div className="chat_footer">
+        <InsertEmoticon />
+        <form>
+          <input placeholder="Type a message" type="text" />
+          <button type="sumbit">Send a message</button>
+        </form>
+        <MicIcon />
       </div>
     </div>
   );
